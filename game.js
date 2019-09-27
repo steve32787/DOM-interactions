c18aff7696db0505ebc5f2dd922f3fa101bb9929
Steve Yoo <steve.yoo@hotmail.com>
2019-09-27 21:45:24 +1200

    update DOM-interaction

--- game.js
+++ game.js
@@ -10,11 +10,15 @@ function start () {
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
+  
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
+  
 function makeGreen (evt) {
   evt.preventDefault()
   evt.target.classList.toggle('green')
@@ -22,15 +26,49 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
+
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
   var totals = {
     blue: 0,
+    blue: 1,
+    blue: 2,
+    blue: 3,
+    blue: 4,
+    blue: 5,
+    blue: 6,
+    blue: 7,
+    blue: 8,
     green: 0,
-    invisible: 0
+    green: 1,
+    green: 2,
+    green: 3,
+    green: 4,
+    green: 5,
+    green: 6,
+    green: 7,
+    green: 8,
+    invisible: 0,
+    invisible: 1,
+    invisible: 2,
+    invisible: 3,
+    invisible: 4,
+    invisible: 5,
+    invisible: 6,
+    invisible: 7,
+    invisible: 8
   }
+ 
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
@@ -41,5 +79,6 @@ function updateCounts () {
 function displayTotals (totals) {
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
+
   }
 }
